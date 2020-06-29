1dfbd8b94ab4c61cde2aa981f97a792ce54070ed
Felix Achter <felix.achter@achter-beratung.de>
2020-06-29 13:38:41 +0200

    timeViewModel

--- src/main/java/com/fhws/zeiterfassung/models/TimeViewModel.java
+++ src/main/java/com/fhws/zeiterfassung/models/TimeViewModel.java
@@ -1,8 +1,14 @@
 package com.fhws.zeiterfassung.models;
 
+import java.time.LocalDateTime;
+
 public class TimeViewModel {
 
     public Long id;
-//    public
-    
+    public LocalDateTime startTime;
+    public LocalDateTime endTime;
+    public String beschreibung;
+    public KundenViewModel kundenViewModel;
+    public ProjektViewModel projektViewModel;
+
 }
