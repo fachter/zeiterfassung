35f34517de3a480979891f1b9bccb64ddbb65d2d
Felix Achter <felix.achter@achter-beratung.de>
2020-07-04 16:24:48 +0200

    delete cors origin from controller

--- src/main/java/com/fhws/zeiterfassung/controllers/WorkedTimeController.java
+++ src/main/java/com/fhws/zeiterfassung/controllers/WorkedTimeController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
-@CrossOrigin(origins = "http://localhost:8082")
 @RestController
 public class WorkedTimeController {
 
