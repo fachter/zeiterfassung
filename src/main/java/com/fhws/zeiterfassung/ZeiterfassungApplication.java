29ace16eaf059116555393ac128c4aaaded8aaf9
Felix Achter <felix.achter@achter-beratung.de>
2020-07-04 13:46:55 +0200

    allow cors localhost and prod link

--- src/main/java/com/fhws/zeiterfassung/ZeiterfassungApplication.java
+++ src/main/java/com/fhws/zeiterfassung/ZeiterfassungApplication.java
@@ -2,12 +2,28 @@ package com.fhws.zeiterfassung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ZeiterfassungApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZeiterfassungApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*")
+                        .allowedOrigins(
+                                "http://localhost:9000",
+                                "https://zeiterfassung.now.sh/");
+            }
+        };
     }
 
 }
